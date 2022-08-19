11fe2ddae17906ffb7e4f481ac56664015f404cb
Erick Bey <erickbey10@gmail.com>
2022-08-19 00:26:02 -0400

    finished the footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -21,6 +21,25 @@ function Footer() {
       <div className="secondary-links__container">
         <div className="secondary-link__item">
           <h3>ABOUT</h3>
+          <div className="about-items__container">
+            <div className="about__column">
+              <p className="about__link">Our Values</p>
+              <p className="about__link">Health and Safety</p>
+              <p className="about__link">All Locations</p>
+            </div>
+            
+            <div className="about__column">
+              <p className="about__link">News and Events</p>
+              <p className="about__link">Cultivate Foundation</p>
+              <p className="about__link">Sustainability</p>
+            </div>
+            
+            <div className="about__column">
+              <p className="about__link">Investors</p>
+              <p className="about__link">Pizzeria Locale</p>
+              <p className="about__link">Do Not Sell My Information</p>
+            </div>
+          </div>
         </div>
 
         <div className="secondary-link__item">
@@ -34,6 +53,15 @@ function Footer() {
           <h3>DOWNLOAD OUR APP</h3>
           <BsApple className='footer__icon'/>
           <DiAndroid className='footer__icon'/>
+        </div>
+      </div>
+
+      <div className="legal-text__container">
+        <div className="legal-links__container">
+          <p className="legal__link">California Transparency in Supply Chain Act</p>
+          <p className="legal__link">Terms of Use</p>
+          <p className="legal__link">Accessibility Statement</p>
+          <p className="legal__link">Privacy Policy</p>
         </div>
       </div>
     </section>
